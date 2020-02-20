18e5cb8267b3a91fa8808c1aac22e7894518005e
sharpened-ferret <clonesupreme@gmail.com>
2020-02-20 20:05:16 +0000

    added comparator for signup times

---
+++ src/SignUpTimeComparator.java
@@ -0,0 +1,27 @@
+import java.util.Comparator;
+
+/**
+ * Implements comparing Library objects, based on their signUpTime values
+ *
+ * File Created: 20/02/2020
+ *
+ * @author Neil Woodhouse
+ * @version 1.0
+ * */
+
+public class SignUpTimeComparator implements Comparator {
+    public int compare(Object o1, Object o2) {
+        Library l1 = (Library)o1;
+        Library l2 = (Library)o2;
+
+        if (l1.signupTime == l2.signupTime) {
+            return 0;
+        }
+        else if (l1.signupTime < l2.signupTime) {
+            return 1;
+        }
+        else {
+            return -1;
+        }
+    }
+}
